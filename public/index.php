5f8fba2c7ad696426d5125026a1ff5f7e0b7a8cd
Albo Vieira <albovieira@gmail.com>
2016-06-23 22:42:46 -0300

    Composer changed.

--- public/index.php
+++ public/index.php
@@ -1,7 +1,15 @@
 <?php
 require_once "../bootstrap.php";
 
-$json = new \albov\JsonObject\Json(['id' => 1, 'status' => 'on']);
+$json = new \albov\JsonObject\Json(
+    [
+        'id' => 1,
+        'status' => 'on',
+        'message' => 'A message',
+        'token' => 'Token for validate'
+    ]
+);
 
-$json->toJson();
-$json->render();
+
+print_r($json->toArray());
+print_r($json->render());
